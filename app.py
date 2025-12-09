14efbf960ea68976b157b5572f035843f506c177
benjabmo <benja.bmo@gmail.com>
2025-12-09 16:05:26 +0000

    rama actualizada

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-import pandas as pd
+import streamlit as st
+import datetime
 import numpy as np
+import pandas as pd
 from modules.financials_functions import portfolio_volatility, portfolio_returns, VaR
 from modules.backend import tickers_by_issuer
 
-if __name__ == '__main__': 
+
+# ======
+# BACKEND
+# ======
+
+def complemento_confianza(confianza_str: str) -> float:
+    '''
+    Recibe un nivel de confianza como string (por ejemplo '95')
+    y retorna 1 - nivel_de_confianza como float (por ejemplo 0.05).
+    '''
+    # Eliminar el símbolo '%' si está presente
+    limpio = confianza_str.strip().replace('%', '')
+
+    valor = float(limpio) / 100 # convertir '95' -> 0.05
+    return 1 - valor
+
+def risk(start:str, end:str, confidence:float) -> pd.DataFrame: 
+    '''
     
+    '''
     # Obtener tickers de ishares
     tickers = tickers_by_issuer(issuer='iShares')
 
@@ -16,12 +36,6 @@ if __name__ == '__main__':
     tickers_rv = tickers[tickers['CATEGORIA']== 'ETF RV']
     list_tickers_rv = list(tickers_rv['TICKER'])
 
-    # Rango de Fechas
-    start = '2024-01-01'
-    end = '2024-12-31'
-
-    # Nivel de Confianza (95%)
-    confidence = 0.05
     lst = []
     for portafolio in [list_tickers_rf, list_tickers_rv]:
         
@@ -53,17 +67,29 @@ if __name__ == '__main__':
     df_final = df_final.sort_values(
         by=f'Value at Risk: {1-confidence}%', 
         ascending=False)
-    print(df_final)
-
-
-
-
-        
-
-
     
-        
+    return df_final
+
+st.title('Programació en Finanzas - Evaluación 2')
+
+# Panel Lateral
+st.sidebar.title('Parametros de Entrada')
+
+st.sidebar.date_input('Fecha de Inicio', '2023-01-01', key='start')
+st.sidebar.date_input('Fecha de Fin', '2025-09-01', key='end')
+
+confidence = st.sidebar.selectbox('Nivel de Confianza', ['95%', '99%'])
+confidence = complemento_confianza(confianza_str=confidence)
+st.session_state.confidence = np.round(confidence,2)
 
 
 
-    
+st.write(st.session_state)
+
+df = risk(
+    start= st.session_state.start, 
+    end = st.session_state.end,
+    confidence= st.session_state.confidence 
+)
+
+st.dataframe(df)
